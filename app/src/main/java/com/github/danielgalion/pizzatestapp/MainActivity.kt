8dd0961fcd216c0e57ae98fe40b002cc1ee690d8
Daniel Galion <daniel.galion94@gmail.com>
2020-09-23 22:38:37 +0200

    Add rounded image in main view
    Use ShapeableImageView with style, to get rounded shape.
    Use Picasso for download image.
    Add internet permission for Picasso usage (and then for communication with API).

--- app/src/main/java/com/github/danielgalion/pizzatestapp/MainActivity.kt
+++ app/src/main/java/com/github/danielgalion/pizzatestapp/MainActivity.kt
@@ -2,13 +2,19 @@ package com.github.danielgalion.pizzatestapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        showLogo()
         title = "RecipeMaster"
+    }
+
+    private fun showLogo() {
+        Picasso.get().load("https://moodup.team/test/pizza1.jpg").into(logo_iv)
     }
 }
