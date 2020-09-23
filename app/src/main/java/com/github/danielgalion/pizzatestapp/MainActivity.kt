e872783229f16b588544f2e5b81dbf3b57b27279
Daniel Galion <daniel.galion94@gmail.com>
2020-09-24 00:56:57 +0200

    Add recipe activity scaffold layout

--- app/src/main/java/com/github/danielgalion/pizzatestapp/MainActivity.kt
+++ app/src/main/java/com/github/danielgalion/pizzatestapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.github.danielgalion.pizzatestapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun showLogo() {
         Picasso.get().load("https://moodup.team/test/pizza1.jpg").into(logo_iv)
+    }
+
+    fun onRecipeFabClick(view: View) {
+        startActivity(Intent(this, RecipeActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/github/danielgalion/pizzatestapp/RecipeActivity.kt
@@ -0,0 +1,21 @@
+package com.github.danielgalion.pizzatestapp
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+class RecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+
+        title = "Pizza Recipe!"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) onBackPressed()
+
+        return super.onOptionsItemSelected(item)
+    }
+}
