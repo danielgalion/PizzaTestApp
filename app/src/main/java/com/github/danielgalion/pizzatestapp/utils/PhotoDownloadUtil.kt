3e3b5363e57e66fc904b6746cae5a5017a674a6d
Daniel Galion <daniel.galion94@gmail.com>
2020-10-01 00:17:10 +0200

    Further minor refactoring

--- app/src/main/java/com/github/danielgalion/pizzatestapp/utils/PhotoDownloadUtil.kt
+++ app/src/main/java/com/github/danielgalion/pizzatestapp/utils/PhotoDownloadUtil.kt
@@ -14,26 +14,29 @@ fun savePhoto(imageView: ImageView?) {
     val bitmap = (imageView?.drawable as BitmapDrawable).bitmap
 
     try {
-        val dir = File(Environment.getExternalStorageDirectory()?.absolutePath + "/DCIM/Camera")
+        trySavePhoto(bitmap)
 
-        if (!dir.isDirectory) dir.mkdirs()
-
-        val fileUri = dir.absolutePath + File.separator + System.currentTimeMillis() + ".jpg"
-        val outputStream = FileOutputStream(fileUri)
-
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-        outputStream.flush()
-        outputStream.close()
-
-        Toast.makeText(
-            MainApplication.getInstance()?.applicationContext, "Pobrano zdjęcie",
-            Toast.LENGTH_LONG
-        ).show()
+        makeToast("Pobrano zdjęcie")
     } catch (e: Exception) {
         e.printStackTrace()
-        Toast.makeText(
-            MainApplication.getInstance()?.applicationContext, "Nie można pobrać zdjęcia, sprawdź uprawnienia",
-            Toast.LENGTH_LONG
-        ).show()
+        makeToast("Nie można pobrać zdjęcia, sprawdź uprawnienia")
     }
+}
+
+private fun trySavePhoto(bitmap: Bitmap) {
+    val dir = File(Environment.getExternalStorageDirectory()?.absolutePath + "/DCIM/Camera")
+
+    if (!dir.isDirectory) dir.mkdirs()
+
+    val fileUri = dir.absolutePath + File.separator + System.currentTimeMillis() + ".jpg"
+    val outputStream = FileOutputStream(fileUri)
+
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+    outputStream.flush()
+    outputStream.close()
+}
+
+private fun makeToast(text: String) {
+    Toast.makeText(MainApplication.getInstance()?.applicationContext, text, Toast.LENGTH_LONG)
+        .show()
 }
