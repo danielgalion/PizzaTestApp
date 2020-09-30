e986bd10c16ca813af640d7e032a4f7b54d2a6e9
Daniel Galion <daniel.galion94@gmail.com>
2020-09-30 23:49:24 +0200

    Add saving photo on click
    With permission check.

--- app/src/main/java/com/github/danielgalion/pizzatestapp/uicontrollers/MainActivity.kt
+++ app/src/main/java/com/github/danielgalion/pizzatestapp/uicontrollers/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.danielgalion.pizzatestapp.uicontrollers
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -8,14 +9,31 @@ import androidx.core.content.ContextCompat
 import com.github.danielgalion.pizzatestapp.R
 import com.github.danielgalion.pizzatestapp.uicontrollers.recipe.RecipeActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val RC_WRITE = 9001
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermissions()
         showLogo()
         title = "RecipeMaster"
+    }
+
+    private fun checkPermissions() {
+        if (!EasyPermissions.hasPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            EasyPermissions.requestPermissions(
+                this,
+                "Aby zapisywać zdjęcia potrzebne są uprawnienia dostępu do pamięci",
+                RC_WRITE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
     }
 
     private fun showLogo() {
